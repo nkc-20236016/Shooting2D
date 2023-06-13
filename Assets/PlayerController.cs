2a0c08ce681a89e65d4e3dcb16c5ce11dc391429
nkc20236016 <nkc23.kohama.hayato@gmail.com>
2023-06-13 15:36:09 +0900

    06/13追加

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,9 +7,12 @@ public class PlayerController : MonoBehaviour
 {
     Vector3 dir = Vector3.zero; // 移動方向を保存する変数
 
+    Animator anim;
+
     void Start()
     {
-        
+        // アニメーターコンポーネントの情報を保存
+        anim = GetComponent<Animator>();
     }
 
 
@@ -29,6 +32,20 @@ public class PlayerController : MonoBehaviour
         pos.y = Mathf.Clamp(pos.y, -5f, 5f);
         transform.position = pos;
 
+        // アニメーション設定
+        if (dir.y == 0)
+        {
+            // アニメーションクリップ【Player】を再生
+            anim.Play("Player");
+        }
+        else if (dir.y == 1)
+        {
+            anim.Play("PlayerL");
+        }
+        else if(dir.y == -1)
+        {
+            anim.Play("PlayerR");
+        }
 
 
     }
